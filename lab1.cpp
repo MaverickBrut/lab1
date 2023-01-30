e64bac8c589651e2bb72c001a9b4501fc4437807
Student <student@lab-walkin-007.cs.csubak.edu>
2023-01-30 11:05:44 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,16 @@
+//Raul Velasco
+//lab-1 jan 24
+//test change on local computer
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 +1;
+    cout << a << endl;
+    cout << "Hello world\n";
+
+
+    return 0;
+}
